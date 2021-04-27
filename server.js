a26a9be9fedf602a75274699fc203f3cc2257380
vagrant <gio_wan@live.ca>
2021-04-27 05:49:49 +0000

    .env for port

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
 const cors = require("cors");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 const bodyParser = require('body-parser');
